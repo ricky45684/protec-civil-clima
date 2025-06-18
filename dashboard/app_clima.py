e6d124103ab7ec1a43ebc33a78498eec4cdb58a2
Ricardo Nicolás <rickyvidela96@gmail.com>
2025-06-18 13:28:48 -0300

    Fix definitivo: conflicto con datetime resuelto

--- dashboard/app_clima.py
+++ dashboard/app_clima.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import os
+from datetime import datetime, timezone
 from datetime import datetime
 # URL del fondo desde GitHub
 FONDO = "https://raw.githubusercontent.com/ricky45684/protec-civil-clima/main/dashboard/assets/fondo/fondo_proteccion.jpg"
